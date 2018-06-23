ef5bab3e07fb2542d0af2e3ea0e5300d637977b0
Jon Richards <jrseti@gmail.com>
2018-06-23 16:54:30 +0000

    Add network initialization
    Add command line parameters to set the destination
    IP/port of the "high-speed" spectra output.
    Since the SNAPs' fabric interface doesn't support ARP,
    the MAC address of the destination also needs to be passed.
    In future this could be grep-ed from the arp-cache
    of the machine running this script.

--- sw/initialize.py
+++ sw/initialize.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
-
 import argparse
 import adc5g
 import casperfpga
 import time
 import numpy as np
+import struct
+import socket
 
 parser = argparse.ArgumentParser(description='Program and initialize a SNAP ADC5G spectrometer',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -22,8 +23,14 @@ parser.add_argument('-e', dest='eth', action='store_true', default=False,
                     help ='Use this flag to switch on Ethernet transmission')
 parser.add_argument('-a', dest='acclen', type=int, default=2**18,
                     help ='Number of 2048-channel spectra to accumulate per dump')
-parser.add_argument('-f', dest='fftshift', type=int, default=0xaaaa,
+parser.add_argument('-f', dest='fftshift', type=int, default=0xffaa,
                     help ='FFT shift schedule')
+parser.add_argument('--destip', dest='destip', type=str, default="10.10.10.10",
+                    help ='Destination IP address to which spectra should be sent')
+parser.add_argument('--destmac', dest='destmac', type=str, default="010203040506",
+                    help ='Destination MAC address to which spectra should be sent. Format should be a hex string')
+parser.add_argument('--destport', dest='destport', type=int, default=10000,
+                    help ='Destination port to which spectra should be sent')
 
 args = parser.parse_args()
 
@@ -44,6 +51,24 @@ snap.write_int('timebase_sync_period', args.acclen * 4096 / 4) # convert to FPGA
 print "Setting FFT shift schedule to %s" % bin(args.fftshift)
 snap.write_int("fft_shift", args.fftshift)
 
+print "Setting destination address to %s:%d" % (args.destip, args.destport)
+snap.write_int("tge_dest_ip", struct.unpack('!I', socket.inet_aton(args.destip))[0])
+snap.write_int("tge_dest_port", args.destport)
+arp_table = np.ones(256, dtype=np.uint32) * int(args.destmac, 16) # Just set all IP's to the same MAC
+snap.gbes.tge_core.set_arp_table(arp_table)
+
+if args.eth:
+    print "Configuring TGE core"
+    # Get MAC address which is auto-configured
+    mac = snap.gbes.tge_core.get_10gbe_core_details()['mac'].mac_int
+    ip_str = socket.gethostbyname(args.host)
+    print "Source IP is %s" % ip_str
+    snap.write('tge_core', socket.inet_aton(ip_str), offset=0x10)
+    #snap.gbes.tge_core.setup(ip, mac, 10000)
+    print "Resetting 10GbE output"
+    snap.write_int("tge_rst", 1)
+    snap.write_int("tge_ctr_rst", 1)
+
 if args.sync:
     print "Re-syncing the design"
     if not args.mansync:
@@ -59,6 +84,11 @@ if args.sync:
         snap.write_int("sync_arm", 0)
         print "  Writing new sync time to snap memory"
         snap.write_int("sync_sync_time", sync_time)
+
+if args.eth:
+    print "Releasing 10GbE reset"
+    snap.write_int("tge_rst", 0)
+    snap.write_int("tge_ctr_rst", 0)
 
 if args.mansync:
    print "  Issuing software sync (you probably only want to do this if you don't have a PPS connected"
